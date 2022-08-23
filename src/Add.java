c87e0a08601b0b2b835df6e0c83bbf5c47c029dd
osingha <osingha@deloitte.com>
2022-08-23 15:32:21 +0530

    First commit

---
+++ src/Add.java
@@ -0,0 +1,14 @@
+
+public class Add {
+
+  public static void main (String [] args){
+    String a = "12";
+    String b = "13";
+    Integer n = null;
+    n = Integer.parseInt(a) +Integer.parseInt(b);
+    System.out.println(a);
+    System.out.println(b);
+    System.out.println(n);
+  }
+
+}
